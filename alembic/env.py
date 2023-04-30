4b62af43f2cae12617091d422c0e9a0f83db373f
DuolanK <duolan.kharitonov@mail.ru>
2023-05-01 01:36:49 +0900

    1st commit

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 
+
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
